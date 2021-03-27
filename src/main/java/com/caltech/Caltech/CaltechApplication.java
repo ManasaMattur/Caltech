69fc3a283a2d22ba146a7d55ac49e2b291f4e7a6
student <manasa6687@gmail.com>
2021-03-27 16:30:54 +0000

    caltech pipeline

---
+++ src/main/java/com/caltech/Caltech/CaltechApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.Caltech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaltechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaltechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/Caltech/CaltechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.Caltech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaltechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
